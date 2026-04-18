bf38a7baf369c04b627f1e7dcede913847cc631d
bensig <1872138+bensig@users.noreply.github.com>
2026-04-17 18:46:20 -0700

    chore: type Astro env vars and ignore wrangler .dev.vars

--- stakemor/src/env.d.ts
+++ stakemor/src/env.d.ts
@@ -1,1 +1,21 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly SUSHI_REFERRER?: string;
+  readonly PUBLIC_PLAUSIBLE_DOMAIN?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare namespace App {
+  interface Locals {
+    runtime: {
+      env: {
+        RESEND_API_KEY?: string;
+        RESEND_AUDIENCE_ID?: string;
+      };
+    };
+  }
+}
